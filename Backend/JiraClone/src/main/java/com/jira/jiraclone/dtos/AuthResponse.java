da0a63817ed84c99dd5264cc2dd1e8d484da7c90
Belghieti <mohamedbelghieti1@gmail.com>
2025-07-08 22:57:39 +0200

    Ajout Strecture generale de projet jira et ajout Model User et UserRole

---
+++ Backend/JiraClone/src/main/java/com/jira/jiraclone/dtos/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.jira.jiraclone.dtos;
+
+public class AuthResponse {
+}
---
+++ Backend/JiraClone/src/main/java/com/jira/jiraclone/entities/User.java
@@ -0,0 +1,30 @@
+package com.jira.jiraclone.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+     private List<UserRole> roles;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}

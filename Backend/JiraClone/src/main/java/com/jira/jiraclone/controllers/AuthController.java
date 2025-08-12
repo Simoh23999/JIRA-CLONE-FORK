456a734d07f9d8fc69b858e6eb0a10c4e7649ad1
Hamza Elhanafy <hamzaelhanafy1@gmail.com>
2025-08-12 09:48:17 +0100

    L'ajout et l'implimentationn du refresh token

--- Backend/JiraClone/src/main/java/com/jira/jiraclone/controllers/AuthController.java
+++ Backend/JiraClone/src/main/java/com/jira/jiraclone/controllers/AuthController.java
@@ -3,8 +3,10 @@ package com.jira.jiraclone.controllers;
 
 import com.jira.jiraclone.dtos.AuthRequest;
 import com.jira.jiraclone.dtos.AuthResponse;
+import com.jira.jiraclone.dtos.RefreshTokenRequest;
 import com.jira.jiraclone.dtos.RegisterRequest;
 import com.jira.jiraclone.services.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +29,29 @@ public class AuthController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
         return authService.authenticate(request);
+    }
+
+    // Endpoint to refresh the JWT token
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
+        AuthResponse response = authService.refreshToken(request);
+
+        if (response.isSuccess()) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+    // Endpoint to logout and invalidate the refresh token
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logout(@RequestBody RefreshTokenRequest request) {
+        AuthResponse response = authService.logout(request);
+
+        if (response.isSuccess()) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
 
